86e8447528967a52d393c8ebce1bfc49087d8246
Manohar <118408671+gmanu21@users.noreply.github.com>
2024-07-15 14:02:12 +0530

    Update UserController.java

--- UserService/src/main/java/com/mn/UserController.java
+++ UserService/src/main/java/com/mn/UserController.java
@@ -4,10 +4,10 @@ package com.mn;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("/users")
 public class UserController {
 
-    @GetMapping("/users")
+    @GetMapping("/user")
     public String getUsers() {
         return "List of users";
     }
